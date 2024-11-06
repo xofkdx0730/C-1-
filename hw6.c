15302d02296fed86e73aa9cffc971a937e0f9f8c
xofkdx0730 <xofkdx@gmail.com>
2024-11-07 00:36:00 +0900

    hw6.c

--- hw6.c
+++ hw6.c
@@ -3,14 +3,31 @@
 
 int main()
 {
-  int arr[5], arrOdd[5], arrEven[5];
-  int Oddlen=0, Evenlen=0, i;
+  int arr[5], i, arrOdd[5], arrEven[5];
+  int Oddlen = 0, Evenlen = 0;
 
   printf("Please input five integers: ");
   scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
-  
-  for (i = 0; i < 5; i++) {
-      if (arr[i] % 2 == 1) {
-          arrOdd[Oddlen] = arr[i];
-          Oddlen += 1;
- 
+
+  for (i=0;i<5;i++)
+	  if (arr[i] % 2 == 0) {
+		  arrEven[Evenlen] = arr[i];
+		  Evenlen += 1;
+	  }
+	  else {
+		  arrOdd[Oddlen] = arr[i];
+		  Oddlen += 1;
+	  }
+
+  printf("\nOdd numbers: ");
+  for (i = 0; i < Oddlen; i++) {
+	  printf("%d ", arrOdd[i]);
+  }
+
+  printf("\nEven numbers: ");
+  for (i = 0; i < Evenlen; i++) {
+  	printf("%d ", arrEven[i]);
+  }
+  printf("\n");
+  return 0;
+}
