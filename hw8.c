7bd3249cb9da587620480a179cd0c93e4c21cc49
xofkdx0730 <xofkdx@gmail.com>
2024-11-19 13:04:14 +0900

    hw8.c

--- hw8.c
+++ hw8.c
@@ -5,21 +5,21 @@
 double StdDev(int *arr)
 {
   double sigma;
-	double sum=0, aver, minus, sqr;
-	int i;
-	for (i = 0; i < 5; i++) {
-		sum += arr[i];
-	}
-	aver = (double)sum / 5;
-	sum = 0;
-	for (i = 0; i < 5; i++) {
-		minus = arr[i] - aver;
-		sqr = pow(minus, 2.0);
-		sum += sqr;
-	}
-	sum /= 5;
-	sigma = sqrt(sum);
-	return sigma;
+  double sum=0, aver, minus, sqr;
+  int i;
+  for (i = 0; i < 5; i++) {
+    sum += arr[i];
+  }
+  aver = (double)sum / 5;
+  sum = 0;
+  for (i = 0; i < 5; i++) {
+    minus = arr[i] - aver;
+    sqr = pow(minus, 2.0);
+    sum += sqr;
+  }
+  sum /= 5;
+  sigma = sqrt(sum);
+  return sigma;
 }
 
 int main()
